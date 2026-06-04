4e3aa015ebe112a7885e0d68445c5c141cc410fe
Htut Myat Ag <htutmyataung549@gmail.com>
2026-06-04 15:15:17 +0630

    feat:change url

--- app/api/backend/route.ts
+++ app/api/backend/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 
-const BACKEND_URL = "http://localhost:8080/api";
-
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'https://bookings-management-system.onrender.com/api';
 // export async function GET(request: Request) {
 //     const {searchParams} = new URL(request.url);
 //     const endpoint = searchParams.get("endpoint");
